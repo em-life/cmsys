12537e97d96d4d8723e92993896d8d0876ec3a68
emlife <1601002008@qq.com>
2016-02-01 19:22:38 +0800

    add

--- Cmd.h
+++ Cmd.h
@@ -1,21 +1,17 @@
-#ifndef  CMSYS_CMD_H
-#define CMSYS_CMD_H
+#ifndef CMD_H
+#define CMD_H
 
-// 支持的命令
-enum CourseCmd
-{
-    // 打印命令帮助信息
+enum CourseCmd {
 	Cmd_PrintHelp = 0,
-    // 打印课程信息
 	Cmd_PrintCourse = 1,
-    // 打印课程数量
 	Cmd_PrintCourseNum = 2,
-    // 打印最长的课程名称
 	Cmd_PrintLongName = 3,
-    // 删除最后一个课程
-	Cmd_RemoveLastCourse = 4,
-    // 退出程序
-	Cmd_Exit = 5,
+	Cmd_RemoveCourseById = 4,
+	Cmd_RemoveCourseByName = 5,
+	Cmd_RemoveLastCourse = 6,
+	Cmd_AddCourse = 7,
+	Cmd_SavaFile = 8,
+	Cmd_Exit = 9,
 };
 
-#endif // CMSYS_CMD_H
+#endif
